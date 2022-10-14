9fdfd6f4ac9fd1b6b4fc1e879e4aa91251f3946d
Gert Marginet <gmarginet@barclab.com>
2022-10-15 01:07:50 +0200

    Update build script

--- build/read-build-info.ps1
+++ build/read-build-info.ps1
@@ -4,3 +4,5 @@ Write-Host $FullPath
 $BuildInfo = Get-Content "$FullPath" | ConvertFrom-Json
 
 Write-Host "Vsix Version = $($BuildInfo.VsixVersion)"
+
+Write-Host "##vso[task.setvariable variable=VsixVersion;]$($BuildInfo.VsixVersion)"
