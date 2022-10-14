05955e1eef777ab80b623abdf42adea33947f418
Gert Marginet <gmarginet@barclab.com>
2022-10-14 22:55:34 +0200

    Add extra build script

---
+++ build/get-version.ps1
@@ -0,0 +1,5 @@
+$FullPath = Resolve-Path $PSScriptRoot\..\src\SqlQueryTools\source.extension.vsixmanifest
+Write-Host $FullPath
+[xml]$content = Get-Content $FullPath
+$version = $content.PackageManifest.Metadata.Identity.Version
+Write-Host "##vso[task.setvariable variable=VsixVersion;]$version"
