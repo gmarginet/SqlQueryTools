c6a794959d425ccf62b2de9063407e93a5c42aca
Gert Marginet <gmarginet@barclab.com>
2022-10-15 00:16:35 +0200

    Update build script

--- build/create-build-Info.ps1
+++
@@ -1,6 +0,0 @@
-$FullPath = Resolve-Path $PSScriptRoot\BuildInfo.json
-Write-Host $FullPath
-
-$BuildInfo = Get-Content "$FullPath" | ConvertFrom-Json
-
-Write-Host "Vsix Version = $($BuildInfo.VsixVersion)"
---
+++ build/create-build-iinfo.ps1
@@ -0,0 +1,13 @@
+$FullPath = Resolve-Path $PSScriptRoot\..\src\SqlQueryTools\source.extension.vsixmanifest
+Write-Host $FullPath
+
+[xml]$content = Get-Content $FullPath
+$version = $content.PackageManifest.Metadata.Identity.Version
+Write-Host "Vsix version = $version"
+
+$BuildInfoJson = @{
+	VsixVersion = $version
+}
+$BuildInfoJson | ConvertTo-Json | Out-File "$PSScriptRoot\BuildInfo.json"
+
+Write-Host "##vso[task.setvariable variable=VsixVersion;]$version"
