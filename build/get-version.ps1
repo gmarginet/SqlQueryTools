e588b91a5cea4a848098357f86898d38397418e0
Gert Marginet <gmarginet@barclab.com>
2022-10-14 23:17:32 +0200

    Update build script

--- build/get-version.ps1
+++ build/get-version.ps1
@@ -2,4 +2,5 @@ $FullPath = Resolve-Path $PSScriptRoot\..\src\SqlQueryTools\source.extension.vsi
 Write-Host $FullPath
 [xml]$content = Get-Content $FullPath
 $version = $content.PackageManifest.Metadata.Identity.Version
+Write-Host "Vsix version = $version"
 Write-Host "##vso[task.setvariable variable=VsixVersion;]$version"
