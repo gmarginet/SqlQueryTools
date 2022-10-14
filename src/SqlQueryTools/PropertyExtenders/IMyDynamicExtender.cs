0fd3823080d7b098e5487070eb3df549e2934df5
Gert Marginet <gmarginet@barclab.com>
2022-10-14 21:22:35 +0200

    Initial commit for Generate poco class

---
+++ src/SqlQueryTools/PropertyExtenders/IMyDynamicExtender.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SqlQueryTools.PropertyExtenders
+{
+    [ComVisible(true)]
+    public interface IMyDynamicExtender
+    {
+        String NewProperty { get; set; }
+    }
+}
